b62047f7bd76c276b546e59744ab159622571882
Andreas Karner <andreas.karner@student.tugraz.at>
2019-05-29 17:53:22 +0200

    BLOCKS-012 implemented first auto test for bloch existence check [AK]

--- scratch-blocks-develop/tests/catblock-tests/block_test.js
+++ scratch-blocks-develop/tests/catblock-tests/block_test.js
@@ -17,17 +17,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
+"use strict";
 
-function test_ForeverExists() {
+const loadPageSync = url => {
+  assertTrue(url != "");
+  assertTrue(url != null);
+  assertTrue(url.length != 0);
 
-  var workspace = new Blockly.Workspace();
+  var req = new XMLHttpRequest();
+  req.open("GET", url, false);
+  req.send(null);
+  if (req.status == 200) {
+    return req.responseText;
+  }
+};
 
-  var text = '<xml xmlns="http://www.w3.org/1999/xhtml"><variables></variables><block type="WhenStartedBrick" id="[o^[E$HNVwUIJ=8I[Un3" x="393" y="76"> <next> <block type="WaitBrick" id="L9cGX`#NH}Vg=wyUMHmI"> <value name="TIME_TO_WAIT_IN_SECONDS"> <shadow type="text" id="p=:9g`L~Pt5%c0fp;Di$"> <field name="TEXT">1</field> </shadow> </value> <next> <block type="SetColorBrick" id="fDgz@lgrr~7pQqh]Upa."> <value name="COLOR"> <shadow type="text" id="qViCQ1)-!*3`s`]oua=7"> <field name="TEXT">0</field> </shadow> </value> </block> </next> </block> </next> </block> </xml>';
+const parseTextToDirArray = text => {
+  assertTrue(text != "");
+  assertTrue(text != null);
+  assertTrue(text.length != 0);
 
-  var xml = Blockly.Xml.textToDom(text);
+  const tmp = document.createElement("html");
+  tmp.innerHTML = text;
+  const files = tmp.getElementsByClassName("display-name");
+  return Object.keys(files).map(idx => files[idx].children[0].text);
+};
 
-  Blockly.Xml.domToWorkspace(xml, workspace);
+function test_AllBlocksExists() {
+  var ref_blocks = parseTextToDirArray(
+    loadPageSync("http://localhost:8080/BlockLibrary/")
+  );
+  const default_toolbox = loadPageSync('http://localhost:8080/scratch-blocks-develop/blocks_vertical/default_toolbox.js');
 
-  console.debug(workspace.getTopBlocks())
+  ref_blocks.filter(item => item !== '../').forEach(block => {
+    console.log(`Check block name: ${block.slice(0, -4)} for existenz`);
+    assertTrue(default_toolbox.indexOf(block.slice(0, -4)) != -1);
+  })
 }
+
+// function test_ForeverExists() {
+
+//   var workspace = new Blockly.Workspace();
+
+//   var text = '<xml xmlns="http://www.w3.org/1999/xhtml"><variables></variables><block type="WhenStartedBrick" id="[o^[E$HNVwUIJ=8I[Un3" x="393" y="76"> <next> <block type="WaitBrick" id="L9cGX`#NH}Vg=wyUMHmI"> <value name="TIME_TO_WAIT_IN_SECONDS"> <shadow type="text" id="p=:9g`L~Pt5%c0fp;Di$"> <field name="TEXT">1</field> </shadow> </value> <next> <block type="SetColorBrick" id="fDgz@lgrr~7pQqh]Upa."> <value name="COLOR"> <shadow type="text" id="qViCQ1)-!*3`s`]oua=7"> <field name="TEXT">0</field> </shadow> </value> </block> </next> </block> </next> </block> </xml>';
+
+//   var xml = Blockly.Xml.textToDom(text);
+
+//   Blockly.Xml.domToWorkspace(xml, workspace);
+
+//   console.debug(workspace.getTopBlocks())
+// }
