a5ac6b5b25670200e59df386b5476bf917938909
whati001 <andreas.karner@student.tugraz.at>
2019-09-06 13:44:31 +0200

    BLOCKS-25 - changed webpack logic to include catblocks_msgs.js as default, postfix not needed files with .bak [AK]

--- scratch-blocks-develop/shim/horizontal.js
+++ scratch-blocks-develop/shim/horizontal.js
@@ -1,1 +1,5 @@
-module.exports = require('imports-loader?Blockly=../shim/blocks_compressed_horizontal-blockly_compressed_horizontal-messages,goog=../shim/blockly_compressed_horizontal.goog!exports-loader?Blockly!../msg/catblocks_msgs');
+// #INFO: Blockly load messages
+//module.exports = require('imports-loader?Blockly=../shim/blocks_compressed_horizontal-blockly_compressed_horizontal-messages,goog=../shim/blockly_compressed_horizontal.goog!exports-loader?Blockly!../msg/catblocks_msgs');
+// #INFO: Catblocks load catblocks messages
+module.exports = require('imports-loader?Blockly=../shim/blocks_compressed_horizontal,goog=../shim/blockly_compressed_horizontal.goog!exports-loader?Blockly!../msg/catblocks_msgs');
+
