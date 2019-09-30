2347eea06ee146eb8f5ca94a458747677d90e611
whati001 <andreas.karner@student.tugraz.at>
2019-09-30 15:42:00 +0200

    BLOCKS-26 - started do refactor code, should be goog eslint complain [AK]

--- scratch-blocks-develop/core/web.js
+++ scratch-blocks-develop/core/web.js
@@ -37,18 +37,18 @@ goog.require('goog.dom.TagName');
 
 
 /**
- * DOMParser instance for Web
- * @type {domparser}
- * @private
- */
-Blockly.Web.domParser = null;
-
-/**
- * Parsing formats for domParser
+ * DomParser_ instance for Web
  * @type {Object}
  * @private
  */
-Blockly.Web.parseFormats = {
+Blockly.Web.domParser_ = null;
+
+/**
+ * Parsing formats for DomParser object
+ * @enum {string}
+ * @private
+ */
+Blockly.Web.parseFormats_ = {
   HTML: 'text/html',
   XML: 'text/xml',
   APP_XML: 'application/xml',
@@ -57,20 +57,20 @@ Blockly.Web.parseFormats = {
 };
 
 /**
- * Defaul options
- * @type {object}
+ * Default options for function calls
+ * @enum {object<}
  * @private
  */
-Blockly.Web.defaultOptions = {
-  injectAllScenesToDiv: {
+Blockly.Web.defaultOptions_ = {
+  injectAllScenes: {
     'test': 'test'
   },
-  createScene: {
+  createSceneContainer: {
     writeHeader: true,
     injectOnClick: true,
     expandable: true
   },
-  createObject: {
+  createObjectContainer: {
     writeHeader: true,
     injectOnClick: true,
     expandable: true
@@ -82,148 +82,152 @@ Blockly.Web.defaultOptions = {
  * @param {string} locale to use for Web, loaded from CatblocksMsgs
  * @public
  */
-Blockly.Web.initBlockly = function (locale = "en_GB") {
-  // #TODO: use the locale from the server
-  Blockly.CatblocksMsgs.setLocale(locale);
-  Blockly.Web.domParser = new DOMParser();
+Blockly.Web.initBlockly = function(locale) {
 
-  // inject css into document head
-  let cssNode = document.createElement('style');
+  // #TODO: use the locale from the server
+  var locale_ = goog.isString(locale) ? locale : "en_GB";
+  Blockly.CatblocksMsgs.setLocale(locale_);
+  Blockly.Web.domParser_ = new DOMParser();
+
+  // injectAllScenes css into document head
+  var cssNode = document.createElement('style');
   document.head.insertBefore(cssNode, document.head.firstChild);
 
-  let cssText = document.createTextNode(Blockly.Web.CSS_CONTENT.join('\n'));
+  var cssText = document.createTextNode(Blockly.Web.CSS_CONTENT.join('\n'));
   cssNode.appendChild(cssText);
-}
+};
 
 /**
  * Parse xml for Web into catblocks dom object
  * @param {string} xml to parse into catblocks dom object
- * @return {dom} xml object
+ * @return {Element} xml object
  * @public
  */
-Blockly.Web.xmlToDom = function (xml) {
-  let xmlDoc = Blockly.Web.domParser.parseFromString(xml, Blockly.Web.parseFormats.XML);
+Blockly.Web.xmlToDom = function(xml) {
+  var xmlDoc = Blockly.Web.domParser_.parseFromString(xml, Blockly.Web.parseFormats_.XML);
   if (xmlDoc.getElementsByTagName('parsererror').length > 0) {
     console.error("Failed to parse Web xml");
     return undefined;
   }
   return xmlDoc;
-}
+};
 
 /**
  * Transform dom xml, execute actions define in options
- * @param {dom} xml to transform
+ * @param {Element|string} xml to transform
  * @param {object} tagActions to map against tag elements
- * @return {dom} prepared xml object
  * @private
  */
-Blockly.Web.transformXml_ = function (xml, tagActions) {
+Blockly.Web.transformXml_ = function(xml, tagActions) {
   // TODO: may move into xml.js file
-  let xmlDom = goog.isString(xml) ? Blockly.Web.xmlToDom(xml) : xml;
+  var xmlDom = goog.isString(xml) ? Blockly.Web.xmlToDom(xml) : xml;
   // run actions against xmlDom
-  Object.keys(tagActions).forEach(tagName => {
-    // TODO: excute based on actions
-    let actions = tagActions[tagName];
-    let nodes = xmlDom.getElementsByTagName(tagName);
-    for (let inodes = 0; inodes < nodes.length; inodes++) {
+  for (var iaction = 0; iaction < tagActions.length; iaction++) {
+    var tagName = tagActions[iaction];
+    var nodes = xmlDom.getElementsByTagName(tagName);
+    for (var inodes = 0; inodes < nodes.length; inodes++) {
       nodes[inodes].removeAttribute('id');
       nodes[inodes].removeAttribute('x');
       nodes[inodes].removeAttribute('y');
     }
-  });
-}
+  }
+};
 
 /**
- * Create new read only workspace and inject it into container
- * @param {dom} container to from scene where we should inject the workspace
- * @param {integer} blockscale block scale for workspace
+ * Create new read only workspace and injectAllScenes it into container
+ * @param {Element} container to from scene where we should injectAllScenes the workspace
+ * @param {number} blockscale block scale for workspace
  * @returns {workspace} created workspace object
  * @private
  */
-Blockly.Web.createReadonlyWorkspace_ = function (container, blockscale = 0.75) {
+Blockly.Web.createReadonlyWorkspace_ = function(container, blockscale) {
   if (container.tagName !== goog.dom.TagName.DIV.tagName_) {
     console.error("Please provide a div as container - createSceneWorkspace");
   }
 
+  var blockscale_ = goog.isNumber(blockscale) ? blockscale : 0.75;
   // force to use the entire width
   // TODO: remove before fly
   //container.style.width = "100%";
   //container.style.height = "1000px";
 
-  // inject a new workspace, we will reuse this one for all scenes
-  let workspace = Blockly.inject(container, {
+  // injectAllScenes a new workspace, we will reuse this one for all scenes
+  var workspace = Blockly.inject(container, {
     readOnly: true,
     media: './../../media/',
     zoom: {
       controls: false,
       wheel: false,
-      startScale: blockscale
+      startScale: blockscale_
     }
   });
   return workspace;
-}
+};
 
 /**
  * Clean workspace, reset size to 0 and remove all blocks
  * @param {workspace} workspace to clean
  * @public
  */
-Blockly.Web.clearSceneWorkspace = function (workspace) {
-  let injectContainer = workspace.blockDragSurface_.container_.parentElement;
-  let mainBlock = injectContainer.getElementsByTagName('svg')[0];
+Blockly.Web.clearSceneWorkspace = function(workspace) {
+  var injectContainer = workspace.blockDragSurface_.container_.parentElement;
+  var mainBlock = injectContainer.getElementsByTagName('svg')[0];
   workspace.clear();
   injectContainer.setAttribute('height', '0px');
   mainBlock.setAttribute('height', '0px');
-}
+};
 
 
 /**
  * Load new xml to workspace, clean first if not already done
- * @param {object} block either String or DOM XML with all the scene blocks
+ * @param {Element|string} xml to load into workspace
  * @param {workspace} workspace to which we should load the xml
  * @param {boolean} erase workspace or not
  * @private
  */
-Blockly.Web.addBlockXmlToWorkspace_ = function (xml, workspace, erase = true) {
+Blockly.Web.addBlockXmlToWorkspace_ = function(xml, workspace, erase) {
   // get DOM xml, either convert or directly
-  let xmlDom = goog.isString(xml) ? Blockly.Xml.textToDom(xml) : xml;
+  var xmlDom = goog.isString(xml) ? Blockly.Xml.textToDom(xml) : xml;
+  var cleanWorkspace = goog.isBoolean(erase) ? erase : true;
 
   // some dom nodes we need to deal with
   console.log(workspace);
 
-  let injectContainer = workspace.blockDragSurface_.container_.parentElement;
+  var injectContainer = workspace.blockDragSurface_.container_.parentElement;
   workspace.getInjectionDiv().id = workspace.id;
-  let blockSvg = injectContainer.getElementsByTagName('svg')[0];
+  var blockSvg = injectContainer.getElementsByTagName('svg')[0];
 
-  let heightOffset = injectContainer.clientHeight === 0 ? 50 : injectContainer.clientHeight;
-  //let workspaceWidth = injectContainer.clientWidth;
-  let widthOffset = 20; //workspaceWidth / 2;
+  var heightOffset = injectContainer.clientHeight === 0 ? 50 : injectContainer.clientHeight;
+  //var workspaceWidth = injectContainer.clientWidth;
+  var widthOffset = 20; //workspaceWidth / 2;
 
   // move it awai from the edges
   xmlDom.firstElementChild.setAttribute('x', widthOffset);
   xmlDom.firstElementChild.setAttribute('y', heightOffset);
   // erase if require
-  if (erase) Blockly.Web.clearSceneWorkspace(workspace);
+  if (cleanWorkspace) {
+    Blockly.Web.clearSceneWorkspace(workspace);
+  }
 
   Blockly.Xml.domToWorkspace(xmlDom, workspace);
-  let gbox = blockSvg.getBBox();
+  var gbox = blockSvg.getBBox();
 
   // remove first the size define by blockly
   blockSvg.setAttribute('height', gbox.height + 100 + 'px');
   blockSvg.style.position = 'inherit';
-}
+};
 
 /**
  * Inject new dom into container with id and class given
- * @param {dom} container dom where to inject the new scene
- * @param {object} tag to inject into container
+ * @param {Element} container dom where to injectAllScenes the new scene
+ * @param {Object} tagName to injectAllScenes into container
  * @param {string} idName to set for the new dom element
  * @param {string} className to set for the new dom element
- * @return {dom} new created subcontainer
+ * @return {Element} new created subcontainer
  * @private
  */
-Blockly.Web.injectNewDom_ = (container, tagName, idName, className) => {
-  let subContainer = goog.dom.createDom(tagName, {
+Blockly.Web.injectNewDom_ = function(container, tagName, idName, className) {
+  var subContainer = goog.dom.createDom(tagName, {
     'id': idName,
     'class': className
   });
@@ -233,59 +237,69 @@ Blockly.Web.injectNewDom_ = (container, tagName, idName, className) => {
 
 /**
  * Inject new scene into cotainer
- * @param {dom} container dom where to inject the new scene
+ * @param {Element} container dom where to injectAllScenes the new scene
  * @param {string} sceneName mapped to id from the new dom
- * @return {dom} new created subcontainer
+ * @param {!object<string, object>} options how we should build up the scene container
+ * @return {Element} new created scene container
  * @private
  */
-Blockly.Web.addSceneContainer_ = function (container, sceneName, options = Blockly.Web.defaultOptions.createScene) {
-  let sceneContainer = Blockly.Web.injectNewDom_(container, goog.dom.TagName.DIV, sceneName, 'catblocks-scene');
+Blockly.Web.addSceneContainer_ = function(container, sceneName, options) {
+  // TODO(add some code which parse done the default options and generate a new object)
+  console.log(options);
+  var options_ = options === undefined ? Blockly.Web.defaultOptions_.createSceneContainer : options;
+  var sceneContainer = Blockly.Web.injectNewDom_(container, goog.dom.TagName.DIV, sceneName, 'catblocks-scene');
 
-  let sceneHeader = null;
-  if (options.writeHeader) {
-    sceneHeader = Blockly.Web.injectNewDom_(sceneContainer, goog.dom.TagName.DIV, `${sceneName}-header`, 'catblocks-scene-header');
-    let sceneText = Blockly.Web.injectNewDom_(sceneHeader, goog.dom.TagName.H3, ``, 'catblocks-scene-text');
-    sceneText.textContent = `Scene - ${sceneName}`;
+  var sceneHeader = null;
+  if (options_.writeHeader) {
+    sceneHeader = Blockly.Web.injectNewDom_(sceneContainer, goog.dom.TagName.DIV, sceneName + '-header', 'catblocks-scene-header');
 
-    if (options.expandable) {
-      sceneText.addEventListener('click', Blockly.Web.codeClickHandler);
+    var sceneText = Blockly.Web.injectNewDom_(sceneHeader, goog.dom.TagName.H3, '', 'catblocks-scene-text');
+    sceneText.textContent = 'Scene - ' + sceneName;
+
+    if (options_.expandable) {
+      sceneText.addEventListener('click', Blockly.Web.codeClickHandler_);
     }
   }
 
   return sceneContainer;
-}
+};
 
 /**
  * Inject new object into cotainer
- * @param {dom} container dom where to inject the new scene
+ * @param {Element} container dom where to injectAllScenes the new scene
  * @param {string} objectName mapped to id from the new dom
- * @return {workspace} new create workspace for object
+ * @param {!object<string, object>} options how we should build up the scene container
+ * @return {Element} new created object container
  * @private
  */
-Blockly.Web.addObjectContainer_ = function (container, objectName, options = Blockly.Web.defaultOptions.createObject) {
-  let objectContainer = Blockly.Web.injectNewDom_(container, goog.dom.TagName.DIV, objectName, 'catblocks-object');
+Blockly.Web.addObjectContainer_ = function(container, objectName, options) {
+  // TODO(add some code which parse done the default options and generate a new object)
+  var options_ = options === undefined ? Blockly.Web.defaultOptions_.createObjectContainer : options;
 
-  let objectHeader = null;
-  if (options.writeHeader) {
-    objectHeader = Blockly.Web.injectNewDom_(objectContainer, goog.dom.TagName.DIV, `${objectName}-header`, 'catblocks-object-header');
-    let objectText = Blockly.Web.injectNewDom_(objectHeader, goog.dom.TagName.H4, ``, 'catblocks-object-text');
-    objectText.textContent = `Object - ${objectName}`;
+  var objectContainer = Blockly.Web.injectNewDom_(container, goog.dom.TagName.DIV, objectName, 'catblocks-object');
 
-    if (options.expandable) {
-      objectText.addEventListener('click', Blockly.Web.codeClickHandler);
+  var objectHeader = null;
+  if (options_.writeHeader) {
+    objectHeader = Blockly.Web.injectNewDom_(objectContainer, goog.dom.TagName.DIV, objectName + '-header', 'catblocks-object-header');
+    var objectText = Blockly.Web.injectNewDom_(objectHeader, goog.dom.TagName.H4, '', 'catblocks-object-text');
+    objectText.textContent = 'Object - ' + objectName;
+    if (options_.expandable) {
+      objectText.addEventListener('click', Blockly.Web.codeClickHandler_);
     }
   }
-  //let svgContainer = Blockly.Web.injectNewDom_(objectContainer, goog.dom.TagName.DIV, `${objectName}-svg`, 'catblocks-svg');
 
   return objectContainer;
-}
+};
+
 
 /**
  * Inject all catblocks scenes from xml into div
- * @param {dom} div parent node where we should inject it
+ * @param {Element} container dom to inject all loaded scenes
+ * @param {string} xmlString which includes all scenes to inject
+ * @param {Object} options how we should inject all scenes
  * @public
  */
-Blockly.Web.injectAllScenesToDiv = function (container, xmlString, options = Blockly.Web.defaultOptions.injectAllScenesToDiv) {
+Blockly.Web.injectAllScenes = function(container, xmlString, options) {
   if (goog.isString(container)) {
     container = document.getElementById(container) ||
       document.querySelector(container);
@@ -295,49 +309,54 @@ Blockly.Web.injectAllScenesToDiv = function (container, xmlString, options = Blo
     throw 'Error: container is not in current document.';
   }
 
-  let xmlDom = Blockly.Web.xmlToDom(xmlString);
+  // TODO(add some code which parse done the default options and generate a new object)
+  var options_ = options === null ? Blockly.Web.defaultOptions_.injectAllScenes : options;
+  if (options_) {
+    console.error('');
+  }
+
+  var xmlDom = Blockly.Web.xmlToDom(xmlString);
   if (!xmlDom) {
-    console.error('Failed to injectAllScenesToDiv, could not parse xmlString');
+    console.error('Failed to injectAllScenes, could not parse xmlString');
     return;
   }
 
   // iterate over all scenes -> objects -> scripts
-  let scenes = xmlDom.firstChild.children;
-  for (let iscene = 0; iscene < scenes.length; iscene++) {
-    let scene = scenes[iscene];
-    let sceneContainer = Blockly.Web.addSceneContainer_(container, scene.getAttribute('type'));
+  var scenes = xmlDom.firstChild.children;
+  for (var iscene = 0; iscene < scenes.length; iscene++) {
+    var scene = scenes[iscene];
+    var sceneContainer = Blockly.Web.addSceneContainer_(container, scene.getAttribute('type'));
 
 
-    let objects = scene.children;
-    for (let iobject = 0; iobject < objects.length; iobject++) {
-      let object = objects[iobject];
-      let objectName = object.getAttribute('type');
-      let objectContainer = Blockly.Web.addObjectContainer_(sceneContainer, objectName);
-      let svgContainer = objectContainer.lastElementChild;
+    var objects = scene.children;
+    for (var iobject = 0; iobject < objects.length; iobject++) {
+      var object = objects[iobject];
+      var objectName = object.getAttribute('type');
+      var objectContainer = Blockly.Web.addObjectContainer_(sceneContainer, objectName);
+      //var svgContainer = objectContainer.lastElementChild;
 
-      // // transform xml -> 
       Blockly.Web.transformXml_(object, {
         'block': ['rmAtt_id', 'rmAtt_id', 'rmAtt_id']
       });
 
       // // wrap all the childs -> script into one xml for import
-      // let mainNode = goog.dom.createDom('xml', { 'xmlns': 'http://www.w3.org/1999/xhtml' });
+      // var mainNode = goog.dom.createDom('xml', { 'xmlns': 'http://www.w3.org/1999/xhtml' });
       // while (object.childElementCount > 0) {
-      //   let subNode = object.firstElementChild.children;
+      //   var subNode = object.firstElementChild.children;
       //   Object.keys(subNode).forEach(iblock => {
       //     mainNode.appendChild(subNode[iblock]);
       //   });
       //   object.removeChild(object.firstElementChild);
       // }
 
-      // inject new read only workspace
-      let workspace = Blockly.Web.createReadonlyWorkspace_(objectContainer, 0.75);
+      // injectAllScenes new read only workspace
+      var workspace = Blockly.Web.createReadonlyWorkspace_(objectContainer, 0.75);
       //console.log(workspace);
       // load all scripts and update workspace size
       while (object.childElementCount > 0) {
-        let script = object.firstElementChild;
+        var script = object.firstElementChild;
 
-        let blockXml = goog.dom.createDom('xml', { 'xmlns': 'http://www.w3.org/1999/xhtml' });
+        var blockXml = goog.dom.createDom('xml', { 'xmlns': 'http://www.w3.org/1999/xhtml' });
         blockXml.appendChild(script.firstElementChild);
         object.removeChild(script);
 
@@ -348,39 +367,40 @@ Blockly.Web.injectAllScenesToDiv = function (container, xmlString, options = Blo
 
       // TODO: find a better logic, this does not work
       // append svg image into catblocks container to remove all EventHandler and enable scrolling, etc.
-      //let injectDiv = workspace.getParentSvg().parentElement;
+      //var injectDiv = workspace.getParentSvg().parentElement;
       //svgContainer.appendChild(workspace.getParentSvg());
       //objectContainer.removeChild(injectDiv);
     }
   }
   // INFO: remove before fly
   //container.style.display = 'none';
-}
+};
+
+
 
 /**
  * EventHandler for code, expand/collapse scene and object
  * @param {Event} event to handle
  * @private
  */
-Blockly.Web.codeClickHandler = function (event) {
-  let container = event.target.parentElement.parentElement;
+Blockly.Web.codeClickHandler_ = function(event) {
+  var container = event.target.parentElement.parentElement;
   // console.log(container);
 
-  let className = event.target.classList.contains('catblocks-scene-text') ? 'catblocks-object' : 'injectionDiv';
-  let objects = container.getElementsByClassName(className);
-  for (let iobject = 0; iobject < objects.length; iobject++) {
-    let object = objects[iobject];
+  var className = event.target.classList.contains('catblocks-scene-text') ? 'catblocks-object' : 'injectionDiv';
+  var objects = container.getElementsByClassName(className);
+  for (var iobject = 0; iobject < objects.length; iobject++) {
+    var object = objects[iobject];
     object.style.display = object.style.display === 'none' ? 'block' : 'none';
 
     // Blockly does not draw the svg's if the container is display none
     // so we need to call svgResize for each expaned workspace
     if (className === 'injectionDiv') {
-      let workspaceId = object.id;
-      console.log(workspaceId);
+      var workspaceId = object.id;
       Blockly.svgResize(Blockly.Workspace.WorkspaceDB_[workspaceId]);
     }
   }
-}
+};
 
 
 /**
