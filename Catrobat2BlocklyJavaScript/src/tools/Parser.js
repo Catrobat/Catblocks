82a87ee152add1e10e344dbd027cdf0df059ae6a
Clemens Walluschek <clemens.walluschek@student.tugraz.at>
2019-10-14 13:47:30 +0200

    BLOCKS-033 Add Formula Parsing beginning [CW]

--- Catrobat2BlocklyJavaScript/src/tools/Parser.js
+++ Catrobat2BlocklyJavaScript/src/tools/Parser.js
@@ -33,17 +33,43 @@ class Brick{
         this.name = name;
         this.loopOrIfBrickList = [];
         this.elseBrickList = [];
-        this.formula = "";
+        this.formValues = new Map();
     }
 }
 
+class Formula{
+    constructor() {
+        this.value = "";
+        this.left = null;
+        this.right = null;
+    }
+    setLeft(leftBlock){
+        if(this.left === null){
+            this.left = leftBlock;
+        }
+        else{
+            this.left.setLeft(leftBlock);
+        }
+    }
+    setRight(rightBlock){
+        if(this.right === null){
+            this.right = rightBlock;
+        }
+        else{
+            this.right.setRight(rightBlock);
+        }
+    }
+}
+
+let sceneList = [];
 
 function parseFile(xml) {
     let scenes = xml.getElementsByTagName('scene');
     for(let i = 0; i < scenes.length; i++)
     {
-        parseScenes(scenes[i]);
+        sceneList.push(parseScenes(scenes[i]));
     }
+    console.log(sceneList);
 }
 
 function parseScenes(scene) {
@@ -57,6 +83,7 @@ function parseScenes(scene) {
         currentScene.objectList.push(parseObjects(objectList[i]));
     }
     console.log(currentScene);
+    return currentScene;
 }
 
 function parseObjects(object) {
@@ -99,6 +126,15 @@ function parseBrick(brick){
 
     for(let i = 0; i < brick.childNodes.length; i++)
     {
+        if(brick.childNodes[i].nodeName === "formulaList"){
+            let formulaList =  brick.childNodes[i].children;
+            for(let j = 0; j  < formulaList.length; j++){
+                let formula = new Formula();
+                workFormula(formula, formulaList[j]);
+                let attribute = formulaList[j].getAttribute("category");
+                currentBrick.formValues.set(attribute, concatFormula(formula, ""));
+            }
+        }
         if(brick.childNodes[i].nodeName === "ifBranchBricks" || brick.childNodes[i].nodeName === "loopBricks")
         {
             let loopOrIfBrickList = (brick.childNodes[i].children);
@@ -117,4 +153,36 @@ function parseBrick(brick){
         }
     }
     return currentBrick;
+}
+
+function workFormula(formula, input) {
+    for (let i = 0; i < input.childNodes.length; i++) {
+        if (input.childNodes[i].nodeName === "leftChild") {
+            let newFormula = new Formula();
+            formula.setLeft(newFormula);
+            workFormula(newFormula, input.childNodes[i]);
+        }
+        if(input.childNodes[i].nodeName === "rightChild")
+        {
+            let newFormula = new Formula();
+            formula.setRight(newFormula);
+            workFormula(newFormula, input.childNodes[i]);
+        }
+        if(input.childNodes[i].nodeName === "value"){
+            formula.value = input.childNodes[i].childNodes[0].nodeValue;
+        }
+    }
+}
+
+function concatFormula(formula, str){
+    if(formula.left !== null){
+        str = concatFormula(formula.left, str);
+    }
+    //Manage Operators & language strings
+    str += formula.value;
+    str += " ";
+    if(formula.right !== null){
+        str = concatFormula(formula.right, str);
+    }
+    return str;
 }
