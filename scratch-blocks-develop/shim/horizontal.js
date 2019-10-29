5f0adc53bc82e92b6d222a86efac4b396ce0649d
Michael Flucher <flucher@student.tugraz.at>
2019-10-29 11:58:49 +0100

    BLOCKS-045 removed scratch-blocks-develop [MF]

--- scratch-blocks-develop/shim/horizontal.js
+++
@@ -1,3 +0,0 @@
-// #INFO: Catblocks load catblocks messages
-module.exports = require('imports-loader?Blockly=../shim/blocks_compressed_horizontal,goog=../shim/blockly_compressed_horizontal.goog!exports-loader?Blockly!../msg/catblocks_msgs');
-
