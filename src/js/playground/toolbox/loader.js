e2bfb3bfe60630c96f97aac00b4a4e6bb85049d3
Michael Flucher <flucher@student.tugraz.at>
2019-12-08 23:32:48 +0100

    BLOCKS-045 added missing categories and fixed color problem [MF]

--- src/js/playground/toolbox/loader.js
+++ src/js/playground/toolbox/loader.js
@@ -1,9 +1,17 @@
+import "./colours";
 import "./default_toolbox";
 
+import "./blocks/arduino";
 import "./blocks/control";
 import "./blocks/data";
+import "./blocks/drone";
 import "./blocks/event";
+import "./blocks/jumping_sumo";
+import "./blocks/legoEV3";
+import "./blocks/legoNXT";
 import "./blocks/looks";
 import "./blocks/motion";
 import "./blocks/pen";
+import "./blocks/phiro";
+import "./blocks/raspi";
 import "./blocks/sound";
