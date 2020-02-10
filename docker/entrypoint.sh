d581b95e20dea911e9b8210dc588f0e8970f0b7d
whati001 <andreas.karner@student.tugraz.at>
2020-02-10 17:47:45 +0100

    BLOCKS-53 updated entrypoint for docker [AK,AH]

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,16 +4,31 @@
 mkdir -p /home/jest/
 cd /home/jest/
 
-# clone and install repository
-git clone https://github.com/Catrobat/Catblocks.git
-cd Catblocks/
+# clone repository
+git clone "https://github.com/Catrobat/Catblocks.git"
 
+# fetch and checkout the commit which triggered the actions
+cd Catblocks/ 
 git fetch origin "$GITHUB_SHA"
 git checkout "$GITHUB_SHA"
 
+# install everything properly
 yarn install
 yarn run toolbox
 yarn run translate
 
 # run test
 yarn run playground:test
+REPORT="report_$(date +%Y%m%d-%H%M%S).html"
+mv ./jest_html_reporters.html $REPORT
+
+# push report
+TOKEN="$1"
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git add $REPORT
+git commit -m "Add test report" -a 
+
+remote_repo="https://${GITHUB_ACTOR}:${TOKEN}@github.com/Catblocks.git"
+git push "https://${GITHUB_ACTOR}:${TOKEN}@github.com/Catblocks.git" "${GITHUB_SHA}:gh-pages"
+
