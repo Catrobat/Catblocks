2560d14ace8c14bc7584fea782a2651f2bc7857c
GitHub Action <action@github.com>
2020-02-12 14:22:22 +0100

    gh-pages, initiated folder structure and webpack [AK]

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+const presets = [
+  [
+    "@babel/env",
+    {
+      targets: {
+        edge: "16",
+        firefox: "60",
+        chrome: "67",
+        safari: "11.1",
+        ie: "11",
+        node: "current"
+      },
+      useBuiltIns: "usage",
+      debug: true,
+      corejs: 3
+    },
+  ]
+];
+
+module.exports = { presets };
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: devMode ? 'development' : 'production',
+  entry: path.join(__dirname, 'src/js/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMode,
+            }
+          },
+          'css-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/html/index.html'),
+      filename: 'index.html',
+      hash: true
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV', 'TYPE'])
+  ],
+  // watch: true,
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    compress: !devMode,
+    noInfo: true,
+    writeToDisk: !devMode
+  },
+  target: 'web'
+};
