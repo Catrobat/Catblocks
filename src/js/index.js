efd9f3c93a3537c6af3b24ebd48e71ead5d3fff6
GitHub Action <action@github.com>
2020-02-12 18:20:45 +0100

    gh-pages, added logic to inject rows and header [AK]

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,177 @@
+/**
+ * @author andreas.karner@student.tugraz.at
+ * @copyright Catrobat
+ * @description Catrobat report page builder
+ */
+
 import './../css/style.scss';
 
-const API_URL = 'https://api.github.com/repos/Catrobat/Catblocks/contents/reports?ref=gh-pages'
+
+// please keep those links up to date
+const REPO_OWNER = 'Catrobat';
+const REPO_NAME = 'Catblocks';
+const REPO_BRANCH = 'gh-pages';
+const REPO_FOLDER = 'reports';
+
+// dom node ids
+const TABLE_ID = 'test-run-table';
+
 
 /**
- * Load all values from github io folder via github api v3
- * @param {string} url 
+ * Validate if html is rendered proplery
+ * 
+ * @method validateHtml
  */
-const getFiles = async (url = API_URL) => {
-  const res = await fetch(API_URL);
+const validateHtml = () => {
+  const resultTable = document.getElementById(TABLE_ID);
+  if (resultTable === undefined) {
+    return false;
+  }
+  if (resultTable.tHead === undefined) {
+    resultTable.appendChild(document.createElement('thead'));
+  }
+  if (resultTable.tBodies.length === 0) {
+    resultTable.appendChild(document.createElement('tbody'));
+  }
+
+  // TODO: please extend this section here
+
+  return true;
+};
+
+/**
+ * return api request url for github api (https://developer.github.com/v3/)
+ * 
+ * @method prepareGithubApi
+ * @param {string} owner 
+ * @param {string} name 
+ * @param {string} branch 
+ * @param {string} folder 
+ * @returns {string}
+ */
+const prepareGithubApi = (owner = REPO_OWNER, name = REPO_NAME, branch = REPO_BRANCH, folder = REPO_FOLDER) => {
+  return `https://api.github.com/repos/${owner}/${name}/contents/${folder}?ref=${branch}`;
+};
+
+/**
+ * Fetch all reports via github api (https://developer.github.com/v3/)
+ * 
+ * @method fetchReports
+ * @param {string} url 
+ * @returns {object}
+ */
+const fetchReports = async (url) => {
+  if (url === undefined || url === "") {
+    throw Error('No url defined for fetching reports.');
+  }
+
+  const res = await fetch(url);
   const json = await res.json();
-  console.log(json);
+  var idCounter = 1;
   return json.map(file => Object.assign({}, {
+    'id': idCounter++,
+    'branch': '#dbd',
+    'commithash': file['sha'],
+    'datetime': '#dbd',
     'name': file['name'],
-    'path': file['path'],
-    'sha': file['sha']
+    'report': file['path'],
   }));
 };
 
-const newRow = (name, url) => {
-  const row = document.createElement('div');
-  row.setAttribute('class', 'row');
-  const link = document.createElement('a');
-  link.href = url;
-  link.innerText = name;
-  row.appendChild(link);
-  return row;
-};
 
-const injectFiles = async (files) => {
-  const exporer = document.getElementById('explorer');
-  for (const file of await files) {
-    console.log(file);
-    exporer.appendChild(newRow(file.name, file.path));
+/**
+ * Return new DOM row element filled with column values
+ * 
+ * @method newDOMRow
+ * @param {Object} colValues
+ * @param {String} colType
+ * @returns {Element}
+ */
+const newDOMRow = (colValues = {}, colType = 'td') => {
+  const colElement = (colType === 'td' || colType === 'th') ? colType : 'td';
+  const row = document.createElement('tr');
+
+  for (const colValue of colValues) {
+    const col = document.createElement(colElement);
+    row.appendChild(col);
+
+    Object.keys(colValue).forEach(key => {
+      switch (key) {
+        case 'value':
+          col.innerText = colValue[key];
+          break;
+        default:
+          col.setAttribute(key, colValue[key]);
+      }
+    });
   }
+  return row;
+}
+
+/**
+ * Return new DOM thead row
+ * 
+ * @method newDOMTHead
+ * @param {Object} colValues
+ * @returns {Element}
+ */
+const newDOMTHead = (colValues) => {
+  return newDOMRow(colValues, 'th');
+}
+/**
+ * Write empty reports header if no reports exists
+ * 
+ * @method writeEmptyHeader
+ */
+const writeEmptyHeader = () => {
+  const thead = document.getElementById(TABLE_ID).tHead;
+  thead.appendChild(newDOMTHead([{ 'scope': 'col', 'value': 'No reports found, please trigger some Catblocks tests' }]));
 };
 
 /**
- * webpage entry function
+ * Inject all reports into your report table
+ * 
+ * @method reports
+ * @param {Promise} files 
  */
-(() => {
-  console.log("a");
+const injectReportRows = async (reports) => {
+  const table = document.getElementById(TABLE_ID);
+  const thead = table.tHead;
+  const tbody = table.tBodies[0];
+  const reportValues = await reports;
+
+  // write theader
+  const headerRow = newDOMTHead(Object.keys(reportValues[0]).map(colName => Object.assign({}, { 'scope': 'col', 'value': colName })));
+  thead.appendChild(headerRow);
+
+  // write test rows
+  for (const value of reportValues) {
+    const colValue = Object.keys(value).map(colName => Object.assign({}, { 'value': value[colName] }));
+    console.log(colValue);
+    tbody.appendChild(newDOMRow(colValue));
+  }
+};
+
+
+/**
+ * webpage entry function
+ * fetch all reports and inject to the webpage
+ */
+(async () => {
+
+  if (!validateHtml()) {
+    console.error('Valid to validiate all components of the page, please contact some developer.');
+    return;
+  }
+
+  const requestUrl = prepareGithubApi();
+  var reports = fetchReports(requestUrl);
+
+  if (reports.length === 0) {
+    console.warn('No reports found, please verify if this is possible');
+    writeEmptyHeader();
+    return;
+  }
+
+  injectReportRows(reports);
 })();
