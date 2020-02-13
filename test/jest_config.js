e590cdfd99414c868fe49d2112dd443975d4a8a0
whati001 <andreas.karner@student.tugraz.at>
2020-02-13 09:29:13 +0100

    BLOCKS-53 moved test config into test folder, cleanup package scripts [AK]

---
+++ test/jest_config.js
@@ -0,0 +1,28 @@
+/**
+ * Jest configuration file
+ * 
+ * @author andreas.karner@student.tugraz.at
+ * 
+ * @description jest configuraiton file
+ *  use jest-puppeteer for testing
+ *  set global SERVER variable, this one is used by the test files
+ *    please validate that the port and host match up with the @file{jest-puppeteer.js}
+ * 
+ *  added jest-junit reporter for jenkins
+ */
+
+process.env.JEST_PUPPETEER_CONFIG = './test/jest-puppeteer.config.js';
+
+module.exports = {
+  preset: "jest-puppeteer",
+  globals: {
+    SERVER: "http://localhost:8080/"
+  },
+  "reporters": [
+    "default",
+    "jest-html-reporters"
+  ],
+  testMatch: [
+    "**/test/**/*.test.js"
+  ]
+};
