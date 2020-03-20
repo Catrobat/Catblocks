26cb76f92cbd7408981d8b2ab424a2d0adc426d6
Michael Flucher <flucher@student.tugraz.at>
2020-03-20 08:46:19 +0100

    BLOCKS-63 add push to gh-pages [MF,AH]

--- gh-pages-deploy-action/entrypoint.sh
+++ gh-pages-deploy-action/entrypoint.sh
@@ -4,8 +4,31 @@
 Catblocks deploy testpage action
 '
 
-set -e
+# go to checkout@v2 folder
+cd /github/workspace/
 
-# install catblocks and run tests
+# build render
 yarn install
+yarn clean
 yarn render:build
+$RETVALUE=$?
+
+mv ./dist ./../dist
+
+# push token
+GITTOKEN="$1"
+git reset --hard
+git clean -df
+git fetch origin gh-pages
+git checkout gh-pages
+rm ./develop -r ||:
+mv ./../dist ./develop
+
+git config user.email "action@github.com"
+git config user.name "GitHub Action"
+git add ./develop
+git commit -m "deploy current testing page"
+
+git push "https://${GITHUB_ACTOR}:${GITTOKEN}@github.com/Catrobat/Catblocks.git" "gh-pages"
+
+exit $RETVALUE
