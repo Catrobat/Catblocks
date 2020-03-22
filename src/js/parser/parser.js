ce7b508a98d4a02cf6754319f946bf81c0348097
whati001 <andreas.karner@student.tugraz.at>
2020-03-22 18:26:10 +0100

    BLOCKS-70 added simple inProgress spinner [AK]

--- src/js/parser/parser.js
+++ src/js/parser/parser.js
@@ -102,11 +102,11 @@ const escapeXml = (unsafe) => {
   } else {
     return unsafe.replace(/[<>&'"]/g, function(c) {
       switch (c) {
-        case '<': return '&lt;';
-        case '>': return '&gt;';
-        case '&': return '&amp;';
-        case '\'': return '&apos;';
-        case '"': return '&quot;';
+      case '<': return '&lt;';
+      case '>': return '&gt;';
+      case '&': return '&amp;';
+      case '\'': return '&apos;';
+      case '"': return '&quot;';
       }
     });
   }
