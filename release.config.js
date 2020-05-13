046bdbc209f3b75020a28291e086cffee1868350
Michael Flucher <flucher@student.tugraz.at>
2020-05-13 22:36:27 +0200

    BLOCKS-119 implement release module and webpack config [MF]

---
+++ release.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src/js/release.js'),
+  output: {
+    filename: 'CatBlocks.js',
+    path: path.resolve(__dirname, 'release'),
+    libraryTarget: 'var',
+    library: 'CatBlocks'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          // 'postcss-loader',
+          // 'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/html/release.html'),
+      filename: 'index.html',
+      hash: true
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename:  '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+    new CopyPlugin([
+      { from: 'assets', to: 'media' },
+      { from: 'node_modules/blockly/media', to: 'media' },
+      { from: 'i18n/json', to: 'i18n' },
+      { from: 'test/share', to: 'assets/share' },
+      { from: 'favicon.ico', to: 'favicon.ico' }
+    ])
+  ],
+  target: 'web',
+  devtool: 'source-map',
+};
