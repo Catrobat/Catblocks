e3138a1f4415d4642b7130d50a71b4a6cf397438
Bernhard Prattes <b.prattes@student.tugraz.at>
2021-03-15 18:32:52 +0100

    BLOCKS-249 add brick and update language

--- src/library/js/blocks/categories/user.js
+++ src/library/js/blocks/categories/user.js
@@ -22,5 +22,24 @@ export default {
         name: 'SUBSTACK2'
       }
     ]
+  },
+  ReportBrick: {
+    message0: '%{BKY_USER_BRICK_REPORT}',
+    args0: [
+      {
+        type: 'field_input',
+        name: 'REPORT_BRICK',
+        text: 'unset'
+      },
+      {
+        type: 'field_image',
+        src: `${document.location.pathname}media/info_icon.svg`,
+        height: 24,
+        width: 24,
+        alt: '(i)',
+        flip_rtl: true,
+        name: 'REPORT_BRICK_INFO'
+      }
+    ]
   }
 };
