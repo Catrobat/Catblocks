4e3aaee77a0d59b262d3d954e22cc4d30b4a8e15
Gregor Potthast <potthast@student.tugraz.at>
2022-02-04 19:11:09 +0100

    BLOCKS-283 fix push automatically

--- github_actions/catroid-support-checker-action/entrypoint.sh
+++ github_actions/catroid-support-checker-action/entrypoint.sh
@@ -12,3 +12,16 @@ git clone $CATROID
 
 # run checker & send report
 python3 /checker.py "$WORKDIR" "$1"
+
+GITTOKEN="$1"
+RETVALUE="$?"
+COMMITMSG="Automatically language update."
+
+git config user.email "action@github.com"
+git config user.name "GitHub Action"
+git add i18n/catroid_strings/
+git commit -m $COMMITMSG
+
+git push "https://${GITHUB_ACTOR}:${GITTOKEN}@github.com/Catrobat/Catblocks.git" "gh-pages"
+
+exit $RETVALUE
