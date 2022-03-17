3a3e8bb2dd028e5ee51de8abab0199f08df804fb
Michael Flucher <michael.flucher@b4b-solutions.com>
2022-03-17 12:52:27 +0100

    BLOCKS-299 fix broken gh pages action (#350)

--- github_actions/gh-pages-deploy-action/entrypoint.sh
+++ github_actions/gh-pages-deploy-action/entrypoint.sh
@@ -31,8 +31,9 @@ COMMITMSG="'Deploy: Merged Pull Request #${PRNUMBER}'"
 git config user.email "action@github.com"
 git config user.name "GitHub Action"
 git add ./develop
-git commit -m $COMMITMSG
+git commit -m "$COMMITMSG"
 
 git push "https://${GITHUB_ACTOR}:${GITTOKEN}@github.com/Catrobat/Catblocks.git" "gh-pages"
+RETVALUE=$(($? + $RETVALUE))
 
 exit $RETVALUE
