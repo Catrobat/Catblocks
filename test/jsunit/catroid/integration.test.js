eb42a95dab59e7d65128272d282b0a430252b4cf
P-Kupiec <100196569+P-Kupiec@users.noreply.github.com>
2023-01-21 12:18:05 +0100

    BLOCKS-312 [Adv. mode] Specify color palette, set a dark background of Catblocks (#394)
    BLOCKS-312 set background to dark

--- test/jsunit/catroid/integration.test.js
+++ test/jsunit/catroid/integration.test.js
@@ -53,3 +53,51 @@ describe('Catroid Integration Position tests', () => {
     expect(textToScriptBorderOffset < 7).toBe(true);
   });
 });
+
+describe('Catroid Integration Advanced Mode tests', () => {
+  beforeAll(async () => {
+    await page.goto('http://localhost:8080', {
+      waitUntil: 'networkidle0'
+    });
+    const programXML = fs.readFileSync(path.resolve(__dirname, '../../programs/binding_of_krishna_1_12.xml'), 'utf8');
+    const language = 'en';
+    const rtl = false;
+    const advancedMode = true;
+    await page.evaluate(
+      async (pLanguage, pRTL, pAdvancedMode) => {
+        try {
+          await Test.CatroidCatBlocks.init({
+            container: 'catroid',
+            renderSize: 0.75,
+            language: pLanguage,
+            rtl: pRTL,
+            i18n: '/i18n',
+            shareRoot: '',
+            media: 'media/',
+            noImageFound: 'No_Image_Available.jpg',
+            renderLooks: false,
+            renderSounds: false,
+            readOnly: false,
+            advancedMode: pAdvancedMode
+          });
+        } catch (e) {
+          console.error(e);
+        }
+      },
+      language,
+      rtl,
+      advancedMode
+    );
+    await page.evaluate(async pProgramXML => {
+      await Test.CatroidCatBlocks.render(pProgramXML, 'Main Menu', 'Play', 'd9e76a0d-8f6b-44b4-887d-fdd31b7e5bf1');
+    }, programXML);
+  });
+  test('Background color check', async () => {
+    // debugger;
+    const backgroundColor = await page.evaluate(() => {
+      return document.querySelector('#catroid .blocklySvg').style.backgroundColor;
+    });
+
+    expect(backgroundColor).toBe('rgb(26, 26, 26)');
+  });
+});
